5c4763571f34b38d5e534fef18d437dfe16d869d
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-05-12 10:49:02 +0300

    script to calculate mean (by time) mel-spectrograms for train data

---
+++ calc_sum_mel.py
@@ -0,0 +1,10 @@
+import librosa
+import numpy as np
+import glob
+import os.path
+
+for filename in glob.glob('train/*.mp3'):
+    x, fs = librosa.load(filename)
+    spec = librosa.feature.melspectrogram(x, sr=fs, n_mels=128)
+    spec_sum = np.mean(spec, axis=1)
+    print(os.path.basename(filename), *list(spec_sum), sep="\t")
